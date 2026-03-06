f54132de578b48105223a0cc57c23b6629bd648a
Babacar DMC TOURE <babacardmctoure@MacBook-Pro-de-Babacar-2.local>
2026-03-06 10:26:10 +0000

    deploy app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('HELLO');
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req,res)=>{
+    res.send("Inventory App running 🚀")
+})
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000")
+})
